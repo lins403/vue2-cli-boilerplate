6a4971f5ec6e36c7f8f49286ad80837b25773c25
lins14 <linshan6264@gmail.com>
2021-12-28 22:01:23 +0800

    build(.stylelintrc): supplement setup for validating .vue files

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,7 +1,23 @@
 module.exports = {
-  customSyntax: 'postcss-html',
   extends: ['stylelint-config-recommended-scss', 'stylelint-config-recess-order', 'stylelint-prettier/recommended'],
-  rules: {
-    'selector-pseudo-class-no-unknown': [true, { ignorePseudoClasses: ['global', 'export'], ignorePseudoElements: ['v-deep'] }]
-  }
+  overrides: [
+    {
+      files: ['*.vue', '**/*.vue'],
+      customSyntax: 'postcss-html',
+      rules: {
+        'selector-pseudo-class-no-unknown': [
+          true,
+          {
+            ignorePseudoClasses: ['deep', 'global']
+          }
+        ],
+        'selector-pseudo-element-no-unknown': [
+          true,
+          {
+            ignorePseudoElements: ['v-deep']
+          }
+        ]
+      }
+    }
+  ]
 }
