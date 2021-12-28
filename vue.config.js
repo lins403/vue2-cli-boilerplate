6f3e0d696d456e09104697f98fe3c6ccdc5cf588
lins14 <linshan6264@gmail.com>
2021-12-28 17:02:36 +0800

    build: fix build publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  // publicPath: process.env.APP_BASE_URL,  // actions with npm
-  publicPath: '/', // actions with yarn
+  publicPath: process.env.APP_BASE_URL,
   outputDir: 'dist',
   lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: false
