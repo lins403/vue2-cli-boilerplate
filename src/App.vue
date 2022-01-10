fdaee2bdfbedf2c57951090afaf0bd262fc2c7ed
lins14 <linshan6264@gmail.com>
2022-01-10 17:13:57 +0800

    chore(stylelint): fix styelint resolving error by installing postcss@8 manually
    work error if I use resolution>postcss in package.json

--- src/App.vue
+++ src/App.vue
@@ -12,10 +12,10 @@
 <style lang="scss" scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  text-align: center;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
 #nav {
