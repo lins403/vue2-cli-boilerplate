684c3423917a054a1e25550afe392b63ebf5c1b8
lins14 <linshan6264@gmail.com>
2022-01-19 17:08:01 +0800

    chore(vue.config.js): silence sass Deprecation Warning

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,14 @@ module.exports = {
   publicPath: process.env.VUE_APP_BASE_API,
   outputDir: 'dist',
   lintOnSave: process.env.NODE_ENV === 'development',
-  productionSourceMap: false
+  productionSourceMap: false,
+  css: {
+    loaderOptions: {
+      sass: {
+        sassOptions: {
+          quietDeps: true
+        }
+      }
+    }
+  }
 }
